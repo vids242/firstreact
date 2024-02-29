d32766a0acb25d37443513f9417f4cdba6ffcc65
vids242 <vrajdhaduk246@gmail.com>
2024-02-29 15:08:58 +0530

    5_3_24_v

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn new done work 
+          Learn thay gayu hoooo bhai
         </a>
       </header>
     </div>
