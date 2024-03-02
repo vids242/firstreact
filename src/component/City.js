f851372c3e30ea5405b9099fbce3310ae5edf034
vids242 <vrajdhaduk246@gmail.com>
2024-03-02 10:33:59 +0530

    1_3_hw

--- src/component/City.js
+++ src/component/City.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
 
 class City extends Component {
+    constructor(prop) {
+        super(prop)
+
+        this.state = {
+            name: "Surat",
+            population: "2.8M"
+        }
+    }
+
+    hendalbutton = () => {
+        console.log("vraj");
+        this.setState({
+            name: "Rajkot",
+            population: "1.5M"
+        })
+    }
+
     render() {
         return (
-            <div>
-              City  
-            </div>
+            <>
+                <h1>
+                    City
+                </h1>
+                <h3>Your City Name Is : {this.state.name}</h3>
+                <h4>Your City Population Is : {this.state.population}</h4>
+
+                <button onClick={this.hendalbutton}>Next Country</button>
+            </>
+
         );
     }
 }
