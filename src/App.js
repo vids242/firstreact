dc7cb66c78a393d52bd0bc5eb0db6aece2161f17
vids242 <vrajdhaduk246@gmail.com>
2024-03-04 10:48:55 +0530

    2_3_hw

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ import CityFun from './component/CityFun';
 import CounterFun from './component/CounterFun';
 import Country from './component/Country';
 import CountryFun from './component/CountryFun';
+import PropsEx from './component/PropsEx';
 
 function App() {
   return (
     <div>
       {/* <Country/>
       <City/> */}
-      {/* <CountryFun/>
-      <CityFun/> */}
-      <CounterFun/>
+      <CountryFun/>
+      <CityFun/>
+      {/* <CounterFun/> */}
+      <PropsEx text="hello Vraj"/>
     </div>
   );
 }
---
+++ src/component/PropsEx.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function PropsEx(props) {
+    return (
+        <div>
+            <h1>{props.text}</h1>
+            <h2>My Name Is Vraj</h2>
+        </div>
+    );
+}
+
+export default PropsEx;
