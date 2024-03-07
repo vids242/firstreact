27ba3653e21635b6fee662e6da5fea00e92672b0
vids242 <vrajdhaduk246@gmail.com>
2024-03-07 10:24:05 +0530

    6_3_hw

--- src/containers/Timer/TimerFun.js
+++ src/containers/Timer/TimerFun.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 
 function TimerFun() {
+    //1.
     const [time, setTime] = useState(new Date());
 
     const tick = () => {
@@ -8,13 +9,18 @@ function TimerFun() {
     };
 
     useEffect(() => {
+
+        //3.componentDidMount //4.componentDidUpdate
         const timerRef = setInterval(tick, 1000);
 
+        //5.componentWillUnmount
         return () => {
             clearInterval(timerRef);
         };
     }, []);
 
+
+    //2.
     return (
         <div>
             <h2>Timer</h2>
