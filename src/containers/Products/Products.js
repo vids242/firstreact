6394df4a93817c83dc9fcde6c9ba9c6f8065ddcf
vids242 <vrajdhaduk246@gmail.com>
2024-03-08 09:13:55 +0530

    7_3_hw

--- src/containers/Products/Products.js
+++ src/containers/Products/Products.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
+import { Button, Card, CardBody, CardSubtitle, CardText, CardTitle } from 'reactstrap';
 
 function Products(props) {
     const [product_data, setProduct_data] = useState([]);
+    const [search_data, setSearch_data] = useState("");
 
     const getData = async () => {
         const response = await fetch("https://fakestoreapi.com/products");
@@ -14,18 +16,52 @@ function Products(props) {
         getData()
     }, [])
 
-    return (
-        <div>
-            {
-                product_data.map((v, i) => (
-                    <div>
-                        <img src={v.image} width="100px" height="100px" />
-                        <h2>{v.title}</h2>
-                        <h4>{v.price}</h4>
-                    </div>
+    const hendalSearch = (event) => {
+        console.log(event);
+        setSearch_data(event.target.value);
+        //  console.log(search_data);
+    };
+    // console.log(search_data);
 
-                ))
-            }
+    const Filter_data = product_data.filter((v) =>
+        v.title.includes(search_data)
+    );
+
+    // console.log(Filter_data.title);
+    return (
+        <div className="container">
+            <div className="row">
+                <h2 className="text-center"> Product </h2>
+                <div className="mb-3">
+                    <input
+                        type="text"
+                        placeholder="Search..."
+                        value={search_data}
+                        onChange={hendalSearch}
+                    />
+                </div>
+                {product_data.map((v) => (
+                    <div className="col-md-4 gy-4">
+                        <Card
+                            style={{
+                                width: "18rem",
+                            }}
+                        >
+                            <img alt="Sample" src={v.image} height={"200px"} />
+                            <CardBody>
+                                <CardTitle tag="h5">{v.title.length > 20 ? v.title.substring(0, 20) + "..." : v.title}</CardTitle>
+                                <CardSubtitle className="mb-2 text-muted" tag="h6" >
+                                   $ {v.price}
+                                </CardSubtitle>
+                                <CardText>{v.description.length > 50 ? v.description.substring(0, 50) + "..." : v.description}</CardText>
+                                <Button color="success" outline>
+                                    Add To Cart
+                                </Button>
+                            </CardBody>
+                        </Card>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
