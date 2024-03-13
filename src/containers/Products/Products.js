bcd64c77606bdc1b2f9f0f9e03eed55357587833
vids242 <vrajdhaduk246@gmail.com>
2024-03-13 11:01:25 +0530

    work

--- src/containers/Products/Products.js
+++ src/containers/Products/Products.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Card, CardBody, CardSubtitle, CardText, CardTitle } from 'reactstrap';
+import { Button, Card, CardBody, CardSubtitle, CardText, CardTitle, Spinner } from 'reactstrap';
+
 
 function Products(props) {
     const [product_data, setProduct_data] = useState([]);
@@ -7,6 +8,8 @@ function Products(props) {
     const [sort, setSort] = useState("");
     const [category, setCategory] = useState([]);
     const [selectedCategory, setSelectedCategory] = useState("");
+    const [loading, setLoading] = useState(true);
+
 
 
     const getData = async () => {
@@ -23,6 +26,7 @@ function Products(props) {
             }
         });
         setCategory(uniqCategory);
+        setLoading(false);
 
         // console.log(category);
 
@@ -53,11 +57,10 @@ function Products(props) {
 
         // console.log(category);
         // console.log(selectedCategory);
-        if (selectedCategory === category) {
-            fdata.filter((v) => v.category === selectedCategory);
-            
+        if (selectedCategory) {
+            fdata = fdata.filter((v) => v.category === selectedCategory)
         }
-        console.log(fdata);
+        // console.log(fdata);
 
         return fdata
     }
@@ -66,55 +69,77 @@ function Products(props) {
 
     return (
         <div className="container">
-            <div className="row">
-                <h2 className="text-center"> Product </h2>
-                <div className="mb-3">
-                    <input
-                        type="text"
-                        placeholder="Search..."
-                        value={search_data}
-                        onChange={(event) => setSearch_data(event.target.value)}
-                    />
-                    <select name='sort' onChange={(event) => setSort(event.target.value)}>
-                        <option value={""}>--Select Sort--</option>
-                        <option value={"lh"}>Price : Low to High </option>
-                        <option value={"hl"}>Price : High to Low </option>
-                        <option value={"az"}>Filter : A to Z </option>
-                        <option value={"za"}>Filter : Z to A </option>
-
-                    </select>
-                    {category.map((v) => (
-                        <button
-                            style={{ background: v === selectedCategory ? "green" : "none" }}
-                            onClick={() => setSelectedCategory(v)}
-                        >
-                            {v}
-                        </button>
-                    ))}
-
-                </div>
-                {final_data.map((v) => (
-                    <div className="col-md-4 gy-4">
-                        <Card
-                            style={{
-                                width: "18rem",
-                            }}
-                        >
-                            <img alt="Sample" src={v.image} height={"200px"} />
-                            <CardBody>
-                                <CardTitle tag="h5">{v.title.length > 20 ? v.title.substring(0, 20) + "..." : v.title}</CardTitle>
-                                <CardSubtitle className="mb-2 text-muted" tag="h6" >
-                                    $ {v.price}
-                                </CardSubtitle>
-                                <CardText>{v.description.length > 50 ? v.description.substring(0, 50) + "..." : v.description}</CardText>
-                                <Button color="success" outline>
-                                    Add To Cart
-                                </Button>
-                            </CardBody>
-                        </Card>
+            {
+                loading ? (
+                    <div className="text-center align-item mt-5">
+                        <Spinner color="black" type="grow" style={{ width: "3rem", height: "3rem" }} />
+                        <p>Loading...</p>
                     </div>
-                ))}
-            </div>
+                ) : (
+                    <div className="row">
+                        <h2 className="text-center"> Product </h2>
+
+                        <div className="mb-3">
+                            <input
+                                className='search'
+                                type="text"
+                                placeholder="Search..."
+                                value={search_data}
+                                onChange={(event) => setSearch_data(event.target.value)}
+                            />
+                            <select className='selectsort' name='sort' onChange={(event) => setSort(event.target.value)}>
+                                <option value={""}>----Select Sort ----</option>
+                                <option value={"lh"}>Price : Low to High </option>
+                                <option value={"hl"}>Price : High to Low </option>
+                                <option value={"az"}>Filter : A to Z </option>
+                                <option value={"za"}>Filter : Z to A </option>
+
+                            </select>
+                        </div>
+                        <div >
+                            <Button onClick={() => setSelectedCategory("")}
+                                color="success"
+                                outline
+                                style={{ color: selectedCategory === "" ? "white" : "#757575", background: selectedCategory === "" ? "green" : "none" }}
+
+                            >all</Button>
+                            {category.map((v) => (
+                                <Button className='catogory'
+                                    outline
+                                    style={{ color: selectedCategory === v ? "white" : "#757575", background: selectedCategory === v ? "green" : "none" }}
+                                    onClick={() => setSelectedCategory(v)}
+                                >
+                                    {v}
+                                </Button>
+                            ))}
+                        </div>
+
+
+                        {final_data.map((v) => (
+                            <div className="col-md-4 gy-4">
+                                <Card
+                                    style={{
+                                        width: "18rem",
+                                    }}
+                                >
+                                    <img alt="Sample" src={v.image} height={"200px"} />
+                                    <CardBody>
+                                        <CardTitle tag="h5">{v.title.length > 20 ? v.title.substring(0, 20) + "..." : v.title}</CardTitle>
+                                        <CardSubtitle className="mb-2 text-muted" tag="h6" >
+                                            $ {v.price}
+                                        </CardSubtitle>
+                                        <CardText>{v.description.length > 50 ? v.description.substring(0, 50) + "..." : v.description}</CardText>
+                                        <Button color="success" outline>
+                                            Add To Cart
+                                        </Button>
+                                    </CardBody>
+                                </Card>
+                            </div>
+                        ))}
+                    </div>
+                )
+            }
+
         </div>
     );
 }
